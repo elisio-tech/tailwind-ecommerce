546b9ed18db2d0f0c5088f320d974dcffcbe0266
elisio42 <elisiouiux@gmail.com>
2025-04-16 13:30:50 +0100

    hero update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,16 @@
 export default function Hero() {
   return (
-    <div className='mx-8 sm:mx-auto max-w-4xl'>
-
+    <div className="mx-8 sm:mx-auto max-w-4xl">
+      <div className="grid grid-cols-3 justify-center items-center">
+        <div>
+          <h1 className="text-4xl">
+            Fitted Denim <br /> Jacket
+          </h1>
+          <span>$235</span>
+        </div>
+        <div className="justify-center w-64 h-64 rounded-full bg-zinc-500"></div>
+        <div></div>
+      </div>
     </div>
-  )
+  );
 }
