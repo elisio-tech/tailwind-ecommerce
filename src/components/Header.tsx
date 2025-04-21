d1628685c7df2b55b0a05697760f1c3da85189b3
elisioaugusto <elisiouiux@gmail.com>
2025-04-21 16:36:08 +0100

    make changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,20 +6,20 @@ export default function Header() {
     <header className="fixed top-0 left-0 right-0 ">
       <nav className="max-w-4xl mx-4 sm:mx-auto flex justify-between items-center">
         <Link to="/">
-          <Apple size={28} color="#fff" variant="Bold"/>
+          <Apple size={28} color="#000" variant="Bold" />
         </Link>
 
-        <div className="py-8">
+        <div className="py-8 flex gap-8">
           <ul className="flex justify-center items-center gap-8">
             <li>Home</li>
             <li>Feature</li>
             <li>Specs</li>
           </ul>
-        </div>
 
-        <button>
-          <BagHappy size={24} color="#fff" />
-        </button>
+          <button>
+            <span>Cart{" "} [0]</span>
+          </button>
+        </div>
       </nav>
     </header>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,21 @@
+import { ShoppingBag } from "iconsax-reactjs";
+
 export default function Hero() {
   return (
-    <div className="mx-8 sm:mx-auto max-w-4xl ">
-      <div className="grid place-content-center h-screen relative">
-        Apple
-        <footer className="flex justify-between items-end text-white absolute left-0 bottom-6 right-0">
-          <div className="text-sm">
-            <span>Air pods</span>
-            <h2 className="text-xl mb-2">Introducing The Apple Smart</h2>
-            <p className="text-md">Pod - Orion</p>
+    <div className="bg-zinc-100">
+      <div className="relative grid place-content-center h-screen mx-8 sm:mx-auto max-w-4xl">
+        <h1 className="text-[24rem]">Code</h1>
+        <div>
+          <div className="flex absolute bottom-8 left-0 items-center">
+            <div className="w-10 h-10 border border-zinc-700 flex justify-center items-center rounded-full">
+              {" "}
+              <ShoppingBag size={24} color="#000" />
+            </div>
+            <button className="bg-black px-8 py-3 rounded-4xl text-white">
+              Shop now
+            </button>
           </div>
-
-          <div>
-            <span>Immersive</span>
-          </div>
-        </footer>
+        </div>
       </div>
     </div>
   );
