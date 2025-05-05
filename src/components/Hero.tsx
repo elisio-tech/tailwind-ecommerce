25015ac543a263e6c2a825f7dd0dcad2026eaa06
elisioaugusto <elisiouiux@gmail.com>
2025-05-05 13:46:51 +0100

    make hero 2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,28 +1,13 @@
+
+
 export default function Hero() {
   return (
-    <section className="relative h-screen text-white pt-28">
-      <div className="absolute inset-0 z-0">
-        <img
-          src=""
-          alt="Fashion model"
-          className="object-cover w-full h-full"
-        />
-        <div className="absolute inset-0 bg-black/40" />
+    <div className="">
+      <div>
+        <h1>Find Your Flow</h1>
+        <p>Blade Course Polo</p>
+        <span>Scroll down</span>
       </div>
-
-      <div className="relative z-10 max-w-4xl mx-auto  pt-64">
-        <h1 className="text-4xl sm:text-5xl font-bold leading-tight">
-          Pushing boundaries,
-          <br />
-          one look at a time
-        </h1>
-        <p className="mt-4 text-lg text-gray-200">
-          It's not just fashion — it's a statement, a bold expression of style
-        </p>
-        <button className="mt-6 px-12 py-3 bg-white text-black rounded shadow hover:bg-gray-200 transition">
-          Shop Now
-        </button>
-      </div>
-    </section>
-  );
+    </div>
+  )
 }
