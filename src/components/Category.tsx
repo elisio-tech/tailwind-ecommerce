dd8ab6ca1dfcdee2f8149ef3d5478d9fbce2b44e
elisioaugusto <elisiouiux@gmail.com>
2025-05-07 08:44:07 +0100

    make category done

---
+++ src/components/Category.tsx
@@ -0,0 +1,67 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/swiper-bundle.css";
+
+import {  useRef } from "react";
+import type { Swiper as SwiperType } from "swiper";
+import { products } from "../data/products";
+import { ArrowLeft2, ArrowRight2 } from "iconsax-reactjs";
+
+SwiperCore.use([Navigation]);
+
+export default function Category() {
+  const swiperRef = useRef<SwiperType | null>(null);
+
+  return (
+    <section className="pt-24">
+      <div className="mx-auto lg:max-w-4xl xl:max-w-6xl pb-8">
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-5xl font-semibold mb-8">Category</h1>
+          <div className="flex gap-2">
+            <button
+              className="hover:bg-zinc-800 transition-all cursor-pointer w-14 h-14 flex justify-center items-center rounded-full bg-zinc-300"
+              onClick={() => swiperRef.current?.slidePrev()}
+            >
+              <ArrowLeft2 size={20} color="#fff" />
+            </button>
+            <button
+              className="hover:bg-zinc-800 transition-all cursor-pointer w-14 h-14 flex justify-center items-center rounded-full bg-zinc-300"
+              onClick={() => swiperRef.current?.slideNext()}
+            >
+              <ArrowRight2 size={20} color="#fff" />
+            </button>
+          </div>
+        </div>
+        <Swiper
+          onSwiper={(swiper) => (swiperRef.current = swiper)}
+          spaceBetween={20}
+          slidesPerView={1.2}
+          breakpoints={{
+            640: { slidesPerView: 2 },
+            768: { slidesPerView: 3 },
+            1024: { slidesPerView: 4 },
+          }}
+        >
+          {products.map((product, productIndex) => (
+            <SwiperSlide key={productIndex}>
+              <div className="cursor-pointer mb-4">
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="w-full h-64 object-cover mb-4"
+                />
+                <div>
+                  <h3 className="text-lg">{product.name}</h3>
+                  <h4 className="text-zinc-600 mb-4">
+                    ${product.price.toFixed(2)}
+                  </h4>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+}
