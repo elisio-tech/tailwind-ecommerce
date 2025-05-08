4092dc30f009f975dd520b74632a9d1cedac34ae
elisioaugusto <elisiouiux@gmail.com>
2025-05-08 15:06:41 +0100

    footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,38 @@
+import { Instagram,  Snapchat, Whatsapp, Youtube } from "iconsax-reactjs";
 import { footer } from "../data/footer";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="mt-36 bg-zinc-900">
-      <div className="mx-auto lg:max-w-4xl xl:max-w-6xl flex w-full justify-between">
-        {footer.map((item, i) => (
-          <div key={i} className="mb-18 text-white">
-            <h1 className="text-3xl mb-4 pt-24">{item.title}</h1>
-            <div className="flex flex-col gap-2">
-              {item.links.map((link, i) => (
-                <p key={i}>{link.text}</p>
-              ))}
+    <div>
+      <footer className="mt-36 bg-zinc-100 ">
+        <div className="border-b border-zinc-200 pt-24 mx-auto lg:max-w-4xl xl:max-w-6xl flex  justify-between items-start">
+          {footer.map((item, i) => (
+            <div key={i} className="mb-24">
+              <h1 className="text-3xl mb-4">{item.title}</h1>
+              <div className="flex flex-col gap-2">
+                {item.links.map((link, i) => (
+                  <p key={i}>{link.text}</p>
+                ))}
+              </div>
             </div>
+          ))}
+
+        <Link to={"/"}>
+        <img src="/images/logo1.svg" alt="" />
+        </Link>
+        </div>
+
+        <div className="mx-auto lg:max-w-4xl xl:max-w-6xl py-8 flex justify-between">
+          <h1>© 2025 Ecommerce Fronted. by Elisio Augusto</h1>
+          <div className="flex items-center gap-6">
+            <Whatsapp size={28} color="#000" variant="Bold"/>
+            <Instagram size={28} color="#000" variant="Bold"/>
+            <Youtube size={28} color="#000" variant="Bold"/>
+            <Snapchat size={28} color="#000" variant="Bold"/>
           </div>
-        ))}
-        <h1>Mirror</h1>
-      </div>
-    </footer>
+        </div>
+      </footer>
+    </div>
   );
 }
--- src/data/footer.ts
+++ src/data/footer.ts
@@ -1,30 +1,26 @@
 export const footer = [
-    {
-      title: "Shop",
-      links: [
-        { text: "T-Shirts", href: "/t-shirts" },
-        { text: "Polos", href: "/polos" },
-        { text: "Bottoms", href: "/bottoms" },
-        { text: "Outerwear", href: "/outerwear" },
-        { text: "Accessories & Hats", href: "/accessories" },
-        { text: "All Categories", href: "/all" },
-      ],
-    },
-    {
-      title: "Company",
-      links: [
-        { text: "About Manors", href: "/about" },
-        { text: "Journal", href: "/journal" },
-        { text: "Support", href: "/support" },
-        { text: "Find a clubhouse", href: "/clubhouse" },
-      ],
-    },
-    {
-      title: "Support",
-      links: [
-        { text: "My account", href: "/account" },
-        { text: "Shipping", href: "/shipping" },
-        { text: "Returns", href: "/returns" },
-      ],
-    },
-  ];
+  {
+    title: "Shop",
+    links: [
+      { text: "T-Shirts" },
+      { text: "Polos" },
+      { text: "Bottoms" },
+      { text: "Outerwear" },
+      { text: "Accessories & Hats" },
+      { text: "All Categories" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      { text: "About Manors" },
+      { text: "Journal" },
+      { text: "Support" },
+      { text: "Find a clubhouse" },
+    ],
+  },
+  {
+    title: "Support",
+    links: [{ text: "My account" }, { text: "Shipping" }, { text: "Returns" }],
+  },
+];
