2582cba43228b92380c1188418e4be2de92751b2
elisioaugusto <elisiouiux@gmail.com>
2025-05-08 08:37:01 +0100

    footer

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -7,8 +7,9 @@ export default function Work() {
         {work.map((item, i) => (
           <div
             key={i}
-            className={`px-10 py-28 ${i > 0 ? "border-l border-zinc-300" : ""}`}
+            className={`px-10 py-16 ${i > 0 ? "border-l border-zinc-300" : ""}`}
           >
+            <img className="w-16 mb-8" src={item.img} alt={item.title} />
             <h1 className="text-2xl">{item.title}</h1>
             <p className="lg:text-sm xl:text-lg">{item.text}</p>
           </div>
--- src/data/work.ts
+++ src/data/work.ts
@@ -5,12 +5,12 @@ export const work = [
     text: "We offer next day delivery on all our products shipping to the United Kingdom.",
   },
   {
-    img: "/images/delivery-1.svg",
+    img: "/images/delivery-2.svg",
     title: "Free Mulligans",
     text: "That's a free money back guarantee if you’re not 100% satisfied. No guilt in a mulligan here.",
   },
   {
-    img: "/images/delivery-2.svg",
+    img: "/images/delivery-3.svg",
     title: "Made by Golfers",
     text: "We spend a lot of time on the golf course. So much that we decided to make a brand that produced the highest quality golf wear tested on the links.",
   },
