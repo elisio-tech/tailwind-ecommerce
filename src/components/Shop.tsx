9fe045f81d8573cd7ac2d42a1c78774c11de22bd
elisioaugusto <elisiouiux@gmail.com>
2025-05-08 08:27:22 +0100

    add image

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,14 +1,14 @@
 export default function Shop() {
   return (
-    <div className="relative grid items-center mt-24 h-[60vh]  rounded-2xl lg:max-w-4xl xl:max-w-5xl mx-auto mb-8 bg-[#FF9D23] overflow-visible z-10">
+    <div className="relative grid items-center mt-32 h-[60vh]  rounded-2xl lg:max-w-4xl xl:max-w-5xl mx-auto mb-8 bg-[#FF9D23] overflow-visible z-10">
       
       <div className="px-6 lg:px-12 relative z-20">
         <div className="">
           <h1 className="text-6xl mb-2">
             Flow <br /> Jogger Pants
           </h1>
-          <p className="mb-4">
-            Nike is more than you can feel, it makes you better. $59.00
+          <p className="mb-4 text-zinc-700 font-medium">
+            Nike is more than you can feel, it makes you better. 
           </p>
           <button className="px-8 bg-black text-white py-3 rounded-lg">
             Add to bag
--- src/data/work.ts
+++ src/data/work.ts
@@ -1,13 +1,16 @@
 export const work = [
   {
+    img: "/images/delivery.svg",
     title: "Next Day Delivery",
     text: "We offer next day delivery on all our products shipping to the United Kingdom.",
   },
   {
+    img: "/images/delivery-1.svg",
     title: "Free Mulligans",
     text: "That's a free money back guarantee if you’re not 100% satisfied. No guilt in a mulligan here.",
   },
   {
+    img: "/images/delivery-2.svg",
     title: "Made by Golfers",
     text: "We spend a lot of time on the golf course. So much that we decided to make a brand that produced the highest quality golf wear tested on the links.",
   },
