8580ca0d22716b868fade9ebd61a9603147d627e
elisioaugusto <elisiouiux@gmail.com>
2025-05-10 13:02:48 +0100

    products done

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loyalt from "./loyalt/Loyalt";
 import HomePage from "./pages/HomePage";
 import SignUp from "./components/auth/Sign-up";
+import ProductsPage from "./pages/ProductsPage";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route element={<Loyalt />}>
           <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage />} />
         </Route>
         <Route path="/sign-up" element={<SignUp /> } />
       </Routes>
--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,7 +1,89 @@
-
+import { useState } from "react";
+import { database } from "../data/products";
 
 export default function ProductsPage() {
+  const [activeColor, setActiveColor] = useState(
+    database.map((p) => p.colors[0])
+  );
+
+  const [filter, setFilter] = useState("all");
+
+  const filteredProducts =
+    filter === "all"
+      ? database
+      : database.filter((product) => product.category.includes(filter));
+
   return (
-    <div>ProductsPage</div>
-  )
+    <section className=" mx-auto pt-44 lg:max-w-4xl xl:max-w-6xl relative">
+      <div className="grid grid-cols-[1fr_3fr] gap-8 ">
+        <div>
+          <div className="sticky left-0 top-44">
+            <h2 className="text-xl text-zinc-400 mb-8 ">Filter by Category</h2>
+            <div className="flex flex-col gap-2 text-left ">
+              {["all", ...new Set(database.map((p) => p.category))].map(
+                (category, index) => (
+                  <h2
+                    key={index}
+                    className={`text-xl xl:text-2xl cursor-pointer ${
+                      filter === category ? "text-zinc-900" : "text-zinc-400"
+                    }`}
+                    onClick={() => setFilter(category)}
+                  >
+                    {category}
+                  </h2>
+                )
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-3 gap-4">
+          {filteredProducts.map((product, productIndex) => (
+            <div key={productIndex}>
+              <div className="cursor-pointer mb-4">
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="w-full h-full object-cover mb-4"
+                />
+                <div>
+                  <h3 className="text-lg">{product.name}</h3>
+                  <h4 className="text-zinc-600 mb-4">
+                    ${product.price.toFixed(2)}
+                  </h4>
+                  <div className="flex gap-x-2 ">
+                    {product.colors.map((color, colorIndex) => {
+                      const isActive = activeColor[productIndex] === color;
+                      return (
+                        <div
+                          key={colorIndex}
+                          onClick={() => {
+                            const newColors = [...activeColor];
+                            newColors[productIndex] = color;
+                            setActiveColor(newColors);
+                          }}
+                          className={`w-5 h-5 flex justify-center items-center rounded-full border-2 transition 
+                            ${
+                              isActive
+                                ? "border-black scale-110"
+                                : "border-gray-300"
+                            }`}
+                        >
+                          <button
+                            className="w-3 h-3 rounded-full"
+                            style={{ backgroundColor: color.toLowerCase() }}
+                            aria-label={color}
+                          />
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
